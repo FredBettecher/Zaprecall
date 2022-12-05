32b21d56810f78f6c9bd817600480b279bf3e31c
FredBettecher <fredbettecher@gmail.com>
2022-12-05 17:39:13 -0300

    projeto finalizado v1.1

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import { FooterStyle } from "./FooterStyle";
 
 export default function Footer(props){
     return(
-        <FooterStyle data-identifier="footer">
+        <FooterStyle data-identifier="footer" data-test="footer">
             <h1>{props.counter}/{props.cards.length} Concluídos</h1>
         </FooterStyle>
     );
